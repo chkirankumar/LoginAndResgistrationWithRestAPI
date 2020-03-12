0ae3595c93aeae3fbd0129a9c77f4c3ed07c8990
Kiran <ckirankumar28@gmail.com>
2020-03-12 22:52:13 +0530

    Initial commit

---
+++ app/src/main/java/com/kiran/loginandresgistrationwithrestapi/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.kiran.loginandresgistrationwithrestapi.app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.105/login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.105/login_api/register.php";
+}
